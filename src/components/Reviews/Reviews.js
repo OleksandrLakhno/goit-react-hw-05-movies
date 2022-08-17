433f5ebecf37dd77cdf4857a7ed0080d4c24e704
OleksandrLakhno <baikerstreet221bua@gmail.com>
2022-08-17 19:21:42 +0300

    Update Reviews.js

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -8,29 +8,29 @@ const Reviews = () => {
     const { movieId } = useParams();
     const [reviews, setReviews] = useState(null);
         
-        useEffect(() => {
-     async function fetchReviews() {
+    useEffect(() => {
+        async function fetchReviews() {
             try {
                 const reviews = await getMovieReviews(movieId);
                 setReviews(reviews);
             } catch (error) {
                 toast.error("Reviews no found");
             }
-            }
-            fetchReviews();
-        }, [movieId])
+        };
+        fetchReviews();
+    }, [movieId]);
 
-        return (
+    return (
         <div>
-                {reviews && <ul className={s.list}>
-                    {reviews.results.length > 0 ? reviews.results.map(({author, content}) => <li className={s.item} key={author}>
-                        <h3 className={s.author}>Author: {author}</h3>
-                        <p>{content}</p>
+            {reviews && <ul className={s.list}>
+                {reviews.results.length > 0 ? reviews.results.map(({ author, content }) => <li className={s.item} key={author}>
+                    <h3 className={s.author}>Author: {author}</h3>
+                    <p>{content}</p>
                 </li>) : <p>We don't have any reviews for this movie</p>}
                 
-            </ul> }
-            </div>
-        )
+            </ul>}
+        </div>
+    );
     };
 
     export default Reviews;
